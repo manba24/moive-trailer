e56b82c0505769d195e57459817dff10153fdc1b
mjj <306459374@qq.com>
2018-05-24 14:05:43 +0800

    add child process crawler

--- server/crawler/trailer-list.js
+++ server/crawler/trailer-list.js
@@ -1,58 +1,62 @@
 
 const puppeteer = require('puppeteer')
-
 const url = 'https://movie.douban.com/tag/#/?sort=T&range=0,10&tags='
 
 const sleep = time => new Promise(resolve => {
     setTimeout(resolve, time)
 });
 
-    (async () => {
-        const brower = await puppeteer.launch({
-            args: ['--no-sandbox'],
-            dumpio: false
-        })
+(async () => {
+   
+    const brower = await puppeteer.launch({
+        args: ['--no-sandbox'],
+        dumpio: false
+    })
 
-        const page = await brower.newPage()
-        await page.goto(url, {
-            waitUntil: 'networkidle2'
-        })
+    const page = await brower.newPage()
+    await page.goto(url, {
+        waitUntil: 'networkidle2'
+    })
 
+    await sleep(3000)
+
+    await page.waitForSelector('.more')
+
+    for (let i = 0; i < 1; i++) {
         await sleep(3000)
+        await page.click('.more')
+    }
 
-        await page.waitForSelector('.more')
+    const result = await page.evaluate(() => {
+        var $ = window.$
+        var items = $('.list-wp a')
+        var links = []
 
-        for (let i = 0; i < 1; i++) {
-            await sleep(3000)
-            await page.click('.more')
-        }
+        if (items.length >= 1) {
+            items.each((index, item) => {
+                let it = $(item)
+                let doubanId = it.find('div').data('id')
+                let title = it.find('.title').text()
+                let rate = Number(it.find('.rate').text())
+                let poster = it.find('img').attr('src').replace('s_ratio', 'l_ratio')
 
-        const result = await page.evaluate(() => {
-            var $ = window.$
-            var items = $('.list-wp a')
-            var links = []
-
-            if (items.length >= 1) {
-                items.each((index, item) => {
-                    let it = $(item)
-                    let doubanId = it.find('div').data('id')
-                    let title = it.find('.title').text()
-                    let rate = Number(it.find('.rate').text())
-                    let poster = it.find('img').attr('src').replace('s_ratio', 'l_ratio')
-
-                    links.push({
-                        doubanId,
-                        title,
-                        rate,
-                        poster
-                    })
-
+                links.push({
+                    doubanId,
+                    title,
+                    rate,
+                    poster
                 })
-            }
-            return links
-        })
 
-        brower.close()
-        console.log(result)
+            })
+        }
+        return links
+    })
 
-    })()
+    brower.close()
+  
+    process.send({result})
+    process.exit(0)
+   
+  
+    
+})()
